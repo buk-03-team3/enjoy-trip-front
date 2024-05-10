353b7d2432e162e46b767454c3e84e5d85959c0a
이현수 <znight1020@naver.com>
2024-05-10 15:47:28 +0900

    style: 로그인, 회원가입 페이지

--- enjoy-trip-front/src/router/index.js
+++ enjoy-trip-front/src/router/index.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '@/components/landing/LandingPage.vue'
 import TravelPage from '@/components/travel/TravelPage.vue'
+import AuthPage from '@/components/user/AuthPage.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-          path: '/',
-          name: 'LandingPage',
-          component: LandingPage
+            path: '/',
+            name: 'LandingPage',
+            component: LandingPage
         },
         {
-          path: '/TravelPage',
-          name: 'TravelPage',
-          component :  TravelPage
+            path: '/TravelPage',
+            name: 'TravelPage',
+            component: TravelPage
+        },
+        {
+            path: '/login',
+            name: '/login',
+            component: AuthPage
         }
     ]
 })
