c1816f50831a005672df7ba42fdb284a953b5f8b
jaegyeongKim <dongji_11@naver.com>
2024-05-10 14:06:55 +0900

    style: login 유무에 따라 별개의 nav 보여주는 기능

--- enjoy-trip-front/src/layout/NavBar.vue
+++ enjoy-trip-front/src/layout/NavBar.vue
@@ -2,9 +2,6 @@
     <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-            <!-- <a href="" class="navbar-brand p-0">
-        <img src="./assets/img/ssafy_logo.png" alt="Logo" width="125" height="100">
-    </a> -->
             <a href="" class="navbar-brand p-0">
                 <h1 class="m-0 kakao-regular">
                     <i><img src="../../assets/img/ssafy_logo.png" alt="Logo" width="125" height="125" /></i>Enjoy Trip
@@ -15,12 +12,54 @@
                 <span class="fa fa-bars"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
-                <div class="navbar-nav ms-auto py-0">
-                    <a href="index.html" class="nav-item nav-link" style="font-size: 1.5rem">홈</a>
-                    <a href="about.html" class="nav-item nav-link" style="font-size: 1.5rem">게시판</a>
-                    <a href="blog.html" class="nav-item nav-link" style="font-size: 1.5rem">후기 공유</a>
-                    <a href="contact.html" class="nav-item nav-link" style="font-size: 1.5rem">유저 공간</a>
+                <div class="navbar-nav ms-auto py-0" v-show="!isLogin">
+                    <a href="index.html"  class="nav-item nav-link" style="font-size: 1.5rem">홈</a>
+                    <a href="contact.html" class="nav-item nav-link" style="font-size: 1.5rem">로그인</a> 
+                     <a href="contact.html" class="nav-item nav-link" style="font-size: 1.5rem">회원가입</a> 
+                    <a href="about.html" class="nav-item nav-link" style="font-size: 1.5rem">관광지</a>                   
+                    <a href="blog.html" class="nav-item nav-link" style="font-size: 1.5rem">공지사항</a>
+                    <a href="blog.html" class="nav-item nav-link" style="font-size: 1.5rem">커뮤니티</a>
+                    <a href="contact.html" class="nav-item nav-link" style="font-size: 1.5rem">소모임</a>
                 </div>
+
+                <div class="navbar-nav ms-auto py-0" v-show="isLogin">
+                    <a href="index.html"  class="nav-item nav-link" style="font-size: 1.5rem">홈</a>
+                    <a href="about.html" class="nav-item nav-link" style="font-size: 1.5rem">관광지</a>                   
+                    <a href="blog.html" class="nav-item nav-link" style="font-size: 1.5rem">공지사항</a>
+                    <a href="blog.html" class="nav-item nav-link" style="font-size: 1.5rem">커뮤니티</a>
+                    <a href="contact.html" class="nav-item nav-link" style="font-size: 1.5rem">소모임</a>
+                     <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle active " data-bs-toggle="dropdown"> 이현수 님</a>
+                            <div class="dropdown-menu m-0">
+                                <a href="#" class="dropdown-item"><i class="fas fa-cog me-2"></i> 마이페이지</a>
+                                <a href="#/logout" class="dropdown-item"><i class="fas fa-power-off me-2"></i> 로그아웃</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--
+                                <div class="navbar-nav ms-auto py-0">
+                <a href="index.jsp" class="nav-item nav-link <%=thisPage.equals("home") ? "active":"" %>">Home</a>
+                <a href="regionTour.jsp" class="nav-item nav-link <%=thisPage.equals("regionTour") ? "active":"" %>">지역별 관광지</a>
+                <a href="contentTour.jsp" class="nav-item nav-link <%=thisPage.equals("contentTour") ? "active":"" %>">컨텐츠별 관광지</a>
+                <a href="notice.jsp" class="nav-item nav-link <%=thisPage.equals("notice") ? "active":"" %>">공지사항</a>
+                <a href="community.jsp" class="nav-item nav-link <%=thisPage.equals("community") ? "active":"" %>">공유게시판</a>
+                <%if(memberDto == null){ %>
+                <a href="login.jsp" class="nav-item nav-link <%=thisPage.equals("login") ? "active":"" %>">Login</a>
+                <a href="register.jsp" class="nav-item nav-link <%=thisPage.equals("register") ? "active":"" %>">Register</a>
+                <% } else {%>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">${userDto.name} 님</a>
+                    <div class="dropdown-menu m-0">
+	                    <a href="profile.jsp" class="dropdown-item"><i class="fas fa-cog me-2"></i> Account Settings</a>
+	                    <a href="<%=contextPath %>/logout" class="dropdown-item"><i class="fas fa-power-off me-2"></i> Log Out</a>
+                    </div>
+                </div>
+                <% }%>
+            </div>
+                -->
             </div>
         </nav>
     </div>
@@ -29,4 +68,8 @@
 
 <style scope></style>
 
-<script setup></script>
+<script setup>
+    import { ref } from 'vue'
+    const isLogin = ref(true);
+    
+</script>
