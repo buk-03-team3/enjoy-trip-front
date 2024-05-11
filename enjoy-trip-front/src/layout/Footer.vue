b1052845f1ba03b4dbee9a34a89fc9af322ffc23
jaegyeongkim <dongji_11@naver.com>
2024-05-11 21:12:45 +0900

    style: footer 추가

---
+++ enjoy-trip-front/src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="container-fluid footer justify-content-center mt-5">
+    <div class="container py-5">
+      <div class="d-flex justify-content-center align-items-center">
+        <i class="fas fa-share fa-2x text-white me-2"></i>
+        <a class="btn-square btn btn-primary rounded-circle mx-1" href="#"><i class="fab fa-facebook-f"></i></a>
+        <a class="btn-square btn btn-primary rounded-circle mx-1" href="#"><i class="fab fa-twitter"></i></a>
+        <a class="btn-square btn btn-primary rounded-circle mx-1" href="#"><i class="fab fa-instagram"></i></a>
+        <a class="btn-square btn btn-primary rounded-circle mx-1" href="#"><i class="fab fa-linkedin-in"></i></a>
+      </div>
+      <hr class="my-4"> <!-- 구분선 추가 -->
+      <p class="text-center text-white">Copyright © 2024 kjg lhs in ssafy</p> <!-- 저작권 정보 추가 -->
+    </div>
+  </footer>
+</template>
