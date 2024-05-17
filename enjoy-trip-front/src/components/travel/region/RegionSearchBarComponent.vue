6bd9605f23b36f0a31de12c6dc72c7b9e2ea2197
jaegyeongKim <dongji_11@naver.com>
2024-05-17 14:46:03 +0900

    style: kakapMap, 관광지 페이지 레이아웃 수정

--- enjoy-trip-front/src/components/travel/region/RegionSearchBarComponent.vue
+++ enjoy-trip-front/src/components/travel/region/RegionSearchBarComponent.vue
@@ -8,19 +8,22 @@ travelStore.getSidoList();
 </script>
 <template>
     <!-- Header End -->
+     <div class="mx-auto text-center mb-5 mt-5" style="max-width: 900px">
+        <h5 class="section-title px-3 kakao-bold">Travel</h5>
+        <h1 class="mb-4 kakao-bold">관광지 조회</h1></div>
     <div class="container-fluid search-bar position-relative" >
         <div class="container">
-            <div class="position-relative rounded-pill align-items-center mx-auto p-3" style="background: rgba(19, 53, 123, 0.8)">
+            <div class="position-relative rounded-pill mb-3 align-items-center mx-auto p-3" style="background: rgba(19, 53, 123, 0.8)">
                 <div class="row">
                     <div class="col md-4">
-                        <select v-model="travelStore.sidoObj" class="form-select btn btn-primary rounded-pill dropdown-togglecontentsButton" type="button" aria-expanded="false">
+                        <select v-model="travelStore.sidoObj" class="form-select btn btn-primary rounded-pill dropdown-togglecontentsButton kakao-regular" type="button" aria-expanded="false">
                             <option value="" disabled selected>시도</option>
                             <option v-for="sido in travelStore.sidoList" :value="sido" 
                             :key="sido.sidoCode" v-bind="travelStore.sidoName">{{sido.sidoName}}</option>
                         </select>
                     </div>
                     <div class="col md-4">
-                        <select v-model="travelStore.gugunObj" @click="travelStore.getGugunList()" class="form-select btn btn-primary rounded-pill dropdown-toggle contentsButton" type="button" aria-expanded="false">
+                        <select v-model="travelStore.gugunObj" @click="travelStore.getGugunList()" class="form-select btn btn-primary rounded-pill dropdown-toggle contentsButton kakao-regular" type="button" aria-expanded="false">
                             <option value="" disabled selected>구군</option>
                             <option v-for="gugun in travelStore.gugunList" :value="gugun" 
                             :key="gugun.gugunCode">{{gugun.gugunName}}</option>
@@ -28,15 +31,15 @@ travelStore.getSidoList();
                     </div>
                 </div>
             </div>
-<div class="row contentType" >
-    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[12] }"  value="12" @click="travelStore.contentSelect(12)">관광지</div>
-    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[14] }" value="14" @click="travelStore.contentSelect(14)">문화시설</div>
-    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[15] }" value="15" @click="travelStore.contentSelect(15)">축제공연행사</div>
-    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[25] }" value="25" @click="travelStore.contentSelect(25)">여행코스</div>
-    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[28] }" value="28" @click="travelStore.contentSelect(28)">레포츠</div>
-    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[32] }" value="32" @click="travelStore.contentSelect(32)">숙박</div>
-    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[38] }" value="38" @click="travelStore.contentSelect(38)">쇼핑</div>
-    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center" :class="{ selected: travelStore.selectContent[39] }" value="39" @click="travelStore.contentSelect(39)">음식점</div>
+<div class="row contentType mb-3 pb-3" >
+    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[12] }"  value="12" @click="travelStore.contentSelect(12)">관광지🗽</div>
+    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[14] }" value="14" @click="travelStore.contentSelect(14)">문화시설⛲</div>
+    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[15] }" value="15" @click="travelStore.contentSelect(15)">축제공연행사🎆</div>
+    <div class="col-md-2 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[25] }" value="25" @click="travelStore.contentSelect(25)">여행코스🏖</div>
+    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[28] }" value="28" @click="travelStore.contentSelect(28)">레포츠🏄</div>
+    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[32] }" value="32" @click="travelStore.contentSelect(32)">숙박🏠</div>
+    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[38] }" value="38" @click="travelStore.contentSelect(38)">쇼핑🛍</div>
+    <div class="col-md-1 contents-items d-flex justify-content-center align-items-center kakao-regular" :class="{ selected: travelStore.selectContent[39] }" value="39" @click="travelStore.contentSelect(39)">음식점🍖</div>
 </div>
     <button onclick="#" type="button" class="btn btn-primary rounded-pill col md-4" @click="travelStore.searchTravel()">검색</button>
        </div>
