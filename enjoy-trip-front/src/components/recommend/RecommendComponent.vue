8d21a348f00d1ac9d132ff582e93cd12a9bd8bc3
jaegyeongkim <dongji_11@naver.com>
2024-05-18 23:43:10 +0900

    add: backend로부터 추천여행지 데이터 가져오는 기능 구현

---
+++ enjoy-trip-front/src/components/recommend/RecommendComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {useRecommendStore} from '@/stores/recommendStore'
+import { ref, computed, watch } from 'vue'
+const recommendStore = useRecommendStore();
+recommendStore.recommendWithUserAddr();
+/*
+"attractionId": 2786896,
+            "contentTypeId": 12,
+            "title": "함양 덕전리 마애여래입상(덕전리마애불상)",
+            "addr1": "경상남도 함양군 마천면 덕전길 103-104",
+            "addr2": "",
+            "zipcode": "50055",
+            "tel": "",
+            "first_image": null,
+            "first_image2": null,
+            "readCount": 0,
+            "favoriteCount": 0,
+            "gugunCode": 20,
+            "sidoCode": 36,
+            "latitude": 35.38491904,
+            "longitude": 127.6556447,
+            "mlevel": "6"
+*/
+
+</script>
+
+<template>
+    <ol>
+        <il v-for="(recommendElement, index) in recommendStore.recommendList" :key="index">
+            {{ recommendElement.title }} | {{ recommendElement.addr1 }}
+            <br>
+        </il>
+    </ol>
+
+    <div>==================================</div>
+    <button @click="recommendStore.recommendWithFavoriteAddr">즐겨찾기 주소</button>
+
+    <div>==================================</div>
+    <button @click="recommendStore.recommendWithFavoriteContent">즐겨찾기 컨텐츠</button>
+
+
+</template>
---
+++ enjoy-trip-front/src/stores/recommendStore.js
@@ -0,0 +1,77 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { useAuthStore } from '@/stores/authStore'
+import http from '@/common/axios-config.js'
+import axios from '@/common/axios-config'
+export const useRecommendStore = defineStore('recommendStore', () => {
+    
+    const {authStore}= useAuthStore();
+    //사용자 고유 아이디 얻어오기 
+    const userId =ref('')
+    const getUserId = async () => {
+        let loginObj = {
+            email: authStore.email,
+            password: authStore.password
+        }
+        try {
+            let { data } = await http.post('/auth/login', null, { params: loginObj })
+            if (data.result == 'success') {
+                    userId.value= data.user.userId;
+
+            } else if (data.result == 'fail') {
+                alert('이메일 또는 비밀번호를 확인하세요.')
+            }
+        } catch (error) {
+            console.log('recommend: error : ', error)
+            alert('여행지 추천 과정에서 오류가 발생했습니다.')
+        }
+    }
+    const recommendList = ref([]);
+    // 사용자 주거 지역 근처 추천
+    const recommendWithUserAddr = async () => {
+        await getUserId();
+        let url = `/recommend/${userId.value}`
+
+        try {
+            await axios.get(url).then((response) => {
+                let { data } = response //destructuring 문법.. responce에 data 객체만 빼서 할당받겠다.
+                recommendList.value = data.list;
+                console.log(recommendList.value)
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    //사용자 즐겨찾기한 지역 근처 추천
+    const recommendWithFavoriteAddr = async () => {
+        await getUserId();
+        let url = `/recommend/region/${userId.value}`
+
+        try {
+            await axios.get(url).then((response) => {
+                let { data } = response //destructuring 문법.. responce에 data 객체만 빼서 할당받겠다.
+                recommendList.value = data.list;
+                console.log(recommendList.value)
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    //사용자 즐겨찾기한 컨텐츠 위주 추천 
+    const recommendWithFavoriteContent = async () => {
+        await getUserId();
+        let url = `/recommend/type/${userId.value}`
+
+        try {
+            await axios.get(url).then((response) => {
+                let { data } = response //destructuring 문법.. responce에 data 객체만 빼서 할당받겠다.
+                recommendList.value = data.list;
+                console.log(recommendList.value)
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    return { recommendWithUserAddr , recommendWithFavoriteAddr,recommendWithFavoriteContent, getUserId , recommendList}
+})
