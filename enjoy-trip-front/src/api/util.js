722c9aab8d00325e09196f0ca600da868cf90c4d
jaegyeongKim <dongji_11@naver.com>
2024-05-20 17:26:02 +0900

    feat: 날짜데이터 변환 기능 추가

---
+++ enjoy-trip-front/src/api/util.js
@@ -0,0 +1,7 @@
+export function formatDate(dateString) {
+    const date = new Date(dateString)
+    const year = String(date.getFullYear()).slice(-2)
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}년 ${month}월 ${day}일`
+}
