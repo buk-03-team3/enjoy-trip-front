26fa9f286ff589ca04656c2a0fc95c48b4986412
현수 <znight1020@naver.com>
2024-05-22 01:21:42 +0900

    style: 커뮤니티 글 작성 시 category 추가

--- enjoy-trip-front/src/components/community/item/CommunityFormItem.vue
+++ enjoy-trip-front/src/components/community/item/CommunityFormItem.vue
@@ -20,41 +20,39 @@ import { useRouter } from 'vue-router'
 import { useCommunityStore } from '@/stores/communityStore'
 import { useAuthStore } from '@/stores/authStore'
 
-
-let previousImages = [];
+let previousImages = []
 
 function CustomUploadAdapterPlugin(editor) {
     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-        const uploadAdapter = new UploadAdapter(loader, '');
+        const uploadAdapter = new UploadAdapter(loader, '')
         return {
             upload: () => uploadAdapter.upload()
-        };
-    };
+        }
+    }
 
     editor.model.document.on('change:data', () => {
-        const content = editor.getData();
-        previousImages = detectRemovedImages(content);
-    });
+        const content = editor.getData()
+        previousImages = detectRemovedImages(content)
+    })
 }
 
 function detectRemovedImages(currentContent) {
-    const imageUrlRegex = /<img[^>]+src="([^">]+)"/g;
-    const currentImages = [];
-    let match;
-    
+    const imageUrlRegex = /<img[^>]+src="([^">]+)"/g
+    const currentImages = []
+    let match
+
     while ((match = imageUrlRegex.exec(currentContent)) !== null) {
-        currentImages.push(match[1]);
+        currentImages.push(match[1])
     }
 
-    const removedImages = previousImages.filter(url => !currentImages.includes(url));
-    removedImages.forEach(url => {
-        const imageName = url.substring(url.lastIndexOf('/') + 1); // Extract the correct image name with UUID
-        deleteImageFromServer(imageName);
-    });
+    const removedImages = previousImages.filter((url) => !currentImages.includes(url))
+    removedImages.forEach((url) => {
+        const imageName = url.substring(url.lastIndexOf('/') + 1) // Extract the correct image name with UUID
+        deleteImageFromServer(imageName)
+    })
 
-    return currentImages;
+    return currentImages
 }
-
 
 const ckeditor = CKEditor.component
 const editor = ClassicEditor
@@ -160,33 +158,61 @@ function moveList() {
     router.replace({ name: 'community-list' })
 }
 
+const categories = ['여행 후기', '여행 팁', '맛집', '질문', '기타']
+const categoryBtnText = ref('카테고리를 선택하세요')
+let isActiveCategory = ref(false)
+
+const categoryActiveChange = () => {
+    console.log(isActiveCategory.value)
+
+    isActiveCategory.value = !isActiveCategory.value
+}
+
+const selectCategory = (category) => {
+    categoryBtnText.value = category
+    isActiveCategory.value = false
+    communityStore.community.category = categoryBtnText.value
+}
 </script>
 
 <template>
     <div class="edit-container">
         <form @submit.prevent="onSubmit">
-        <div class="mb-3">
-            <label for="userid" class="form-label">작성자 ID</label>
-            <input type="text" class="form-control " v-model="community.userId" placeholder="작성자ID" disabled style="color: black;"/>
-        </div>
-        <div class="mb-3">
-            <label for="subject" class="form-label">제목</label>
-            <input type="text" class="form-control" v-model="community.title" placeholder="제목" style="color: black;" />
-        </div>
-        
-        <div class="mb-3">
-            <ckeditor :editor="editor" v-model="community.content" :config="editorConfig" style="color: black;"></ckeditor>
-        </div>
-        
-        <br />
-        <div class="col-auto text-center">
-            <button type="submit" class="btn btn-outline-primary mb-3" v-if="type === 'regist'">글 작성</button>
-            <button type="submit" class="btn btn-outline-success mb-3" v-else>글 수정</button>
-            <button type="button" class="btn btn-outline-danger mb-3 ms-1" @click="moveList">목록</button>
-        </div>
-    </form>
+            <div class="mb-3">
+                <label for="userid" class="form-label">작성자 ID</label>
+                <input type="text" class="form-control" v-model="community.userId" placeholder="작성자ID" disabled style="color: black" />
+            </div>
+            <hr />
+            <div class="select-menu">
+                <label for="category" class="form-label">카테고리</label>
+                <button type="button" class="select-btn" @click="categoryActiveChange($event)">
+                    <span class="categoryBtnText">{{ categoryBtnText }}</span>
+                    <i class="bx bx-chevron-down"></i>
+                </button>
+
+                <ul class="options" v-if="isActiveCategory">
+                    <li class="option" v-for="category in categories" :key="category" @click="selectCategory(category, $event)">
+                        <span class="option-text">{{ category }}</span>
+                    </li>
+                </ul>
+            </div>
+            <hr />
+            <div class="mb-3">
+                <input type="text" class="form-control" v-model="community.title" placeholder="제목" style="color: black" />
+            </div>
+            <hr />
+            <div class="mb-3">
+                <ckeditor :editor="editor" v-model="community.content" :config="editorConfig" style="color: black"></ckeditor>
+            </div>
+
+            <br />
+            <div class="col-auto text-center">
+                <button type="submit" class="btn btn-outline-primary mb-3" v-if="type === 'regist'">글 작성</button>
+                <button type="submit" class="btn btn-outline-success mb-3" v-else>글 수정</button>
+                <button type="button" class="btn btn-outline-danger mb-3 ms-1" @click="moveList">목록</button>
+            </div>
+        </form>
     </div>
-    
 </template>
 
 <style scoped>
@@ -211,5 +237,102 @@ function moveList() {
     width: 100px !important;
     margin-right: 5px;
     max-width: 100%;
+}
+
+.select-menu .select-btn {
+    display: flex;
+    height: 3rem;
+    min-width: 15vmax;
+    background: #fff;
+    padding: 20px;
+    font-size: 1vmax;
+    font-weight: 400;
+    border-radius: 8px;
+    align-items: center;
+    cursor: pointer;
+    justify-content: space-between;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+.select-menu .options {
+    position: absolute; /* 변경 */
+    
+    overflow-y: auto;
+    max-height: 12rem;
+    padding: 10px;
+    border-radius: 8px;
+    background: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    animation-name: fadeInDown;
+    -webkit-animation-name: fadeInDown;
+    animation-duration: 0.35s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 0.35s;
+    -webkit-animation-fill-mode: both;
+    width: 14vmax; /* 필요에 따라 너비 조정 */
+    z-index: 1000; /* 가려지지 않도록 z-index 최상단으로 설정 */
+}
+
+.select-menu .options .option {
+    display: flex;
+    height: 3vmax;
+    cursor: pointer;
+    padding: 0 16px;
+    border-radius: 8px;
+    align-items: center;
+    background: #fff;
+}
+.select-menu .options .option:hover {
+    background: #f2f2f2;
+}
+.select-menu .options .option i {
+    font-size: 1.4vmax;
+    margin-right: 0.8vmax;
+}
+.select-menu .options .option .option-text {
+    font-size: 1vmax;
+    color: #333;
+}
+
+.select-btn i {
+    font-size: 1.4vmax;
+    transition: 0.3s;
+}
+
+.select-menu.active .select-btn i {
+    border-color: green;
+    transform: rotate(-180deg);
+}
+.select-menu.active .options {
+    border-color: green;
+    opacity: 0;
+    z-index: 10;
+    animation-name: fadeInUp;
+    -webkit-animation-name: fadeInUp;
+    animation-duration: 0.4s;
+    animation-fill-mode: both;
+    -webkit-animation-duration: 0.4s;
+    -webkit-animation-fill-mode: both;
+}
+
+@keyframes fadeInUp {
+    from {
+        transform: translate3d(0, 30px, 0);
+    }
+    to {
+        border-color: green;
+        transform: translate3d(0, 0, 0);
+        opacity: 1;
+    }
+}
+@keyframes fadeInDown {
+    from {
+        transform: translate3d(0, 0, 0);
+        opacity: 1;
+    }
+    to {
+        border-color: green;
+        transform: translate3d(0, 20px, 0);
+        opacity: 1;
+    }
 }
 </style>
